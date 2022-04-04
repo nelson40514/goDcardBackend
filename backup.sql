816dcda3ca2d2d96979aa8cc59e8de88e53c3a1f
NelsonSu <nelson40514@gmail.com>
2022-04-04 23:16:10 +0800

    Backup remote RDB

--- backup.sql
+++ backup.sql
@@ -1,1 +1,41 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for macos12.2 (arm64)
+--
+-- Host: us-cdbr-east-05.cleardb.net    Database: heroku_2a40d52308e3ec4
+-- ------------------------------------------------------
+-- Server version	5.6.50-log
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `links`
+--
+
+DROP TABLE IF EXISTS `links`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `links` (
+  `Id` char(10) NOT NULL,
+  `ExpireDate` datetime DEFAULT NULL,
+  `OriginalUrl` varchar(256) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `links`
+--
+
+LOCK TABLES `links` WRITE;
+/*!40000 ALTER TABLE `links` DISABLE KEYS */;
+INSERT INTO `links` VALUES ('5dSsgT','2022-04-04 01:55:41','http://www.yahoo.com/'),('MiSFaE','2022-04-04 02:55:41','http://www.yahoo.com/'),('PAdXxA','2022-04-04 01:55:41','http://www.yahoo.com/');
+/*!40000 ALTER TABLE `links` ENABLE KEYS */;
+UNLOCK TABLES;
